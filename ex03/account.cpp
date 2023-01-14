37090e9e3a61cc6c39d8039a44ab584bc6f5943b
Emir <99013427+EmirKymz@users.noreply.github.com>
2023-01-14 19:07:55 +0300

    Delete ex03 directory

--- ex03/account.cpp
+++
@@ -1,8 +0,0 @@
-#include "account.hpp"
-#include <iostream>
-#include <iomanip>
-
-int     Account::_nbAccounts = 0;
-int     Account::_totalAmount = 0;
-int     Account::_totalNbDeposits = 0;
-int     Account::_totalNbWithdrawals = 0;
