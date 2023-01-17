a79136d8010db5387df9c01959a8bc25136a630e
Emircan Kaymaz <ekaymaz@k2m07s06.42kocaeli.com.tr>
2023-01-18 02:12:24 +0300

    CPP MODULES

--- test1.cpp
+++ test1.cpp
@@ -15,7 +15,20 @@ while ((pos = line.find(s2, pos)) != std::string::npos) {
 std::cout << line << std::endl;
 }
 
+/*  float a(0.00000005);
+    std::cout<< std::fixed << a << std::endl;
+    float b(2.54e-3);
+    std::cout << b << std::endl;
+    std::cout.setf(std::ios::fixed);
+    std:: cout << std::scientific << b << std::endl;
+    std::cout.setf(std::ios::scientific | std::ios::fixed);
+    std::cout << b << std::endl;
+    int c = 15;
+    std::cout << std::oct << c << std::endl;
+    std::cout << std::uppercase << std::hex << c << std::endl;
+    std::cout << std::nouppercase;
 
+ */
 /* size_t pos = 0;
 while ((pos = line.find(s2, pos)) != std::string::npos) {
     line[pos] = s3[0];
