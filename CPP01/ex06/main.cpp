872c694cbb1bc09555bf77de1bdf21625a592dad
Emircan Kaymaz <ekaymaz@k2m07s06.42kocaeli.com.tr>
2023-01-18 02:40:34 +0300

    CPP01 finished

---
+++ CPP01/ex06/main.cpp
@@ -0,0 +1,12 @@
+#include "Harl.hpp"
+
+
+
+int main(int ac, char **av)
+{
+    if(ac !=2)
+        return(0);
+    Harl status;
+    std::string harlstatus(av[1]);
+    status.complain(harlstatus);
+}
