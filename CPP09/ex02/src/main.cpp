b37195b7abb437b3fa134b41dc5d67808a347586
Emircan Kaymaz <ekaymaz@k2m05s02.42kocaeli.com.tr>
2023-06-05 18:49:24 +0300

    CPP MODULES

---
+++ CPP09/ex02/src/main.cpp
@@ -0,0 +1,12 @@
+#include "PmergeMe.hpp"
+
+int main(int ac, char **av) {
+    if (ac < 2) {
+        std::cout << "Give me more numbers" << std::endl;
+        return (1);
+    }
+    PmergeMe a(ac, av);
+    a.Info_begin();
+    a.mergeDeque();
+    a.mergeVector();
+}
